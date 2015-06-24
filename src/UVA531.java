98b48c73bdd33180f89160f5cb722d1e48df9beb
Vishal Patel <vis8051@gmail.com>
2015-06-25 05:10:11 +1000

    UVA531

--- src/UVA531.java
+++ src/UVA531.java
@@ -1,11 +1,36 @@
+import java.util.Scanner;
+
 /**
  * Created by vishal on 6/24/15.
  */
 public class UVA531 {
+
     public  static void main(String[] args)
     {
+        String p;
+        String q;
+
+        Scanner reader = new Scanner(System.in);
+
+        String b = reader.nextLine();
+        p = b;
+        while ( true )
+        {
+            b = reader.nextLine();
+            if (b.equals("#")) break;
+            p += b;
+        }
+
+        b = reader.nextLine();
+        q = b;
+        while ( true )
+        {
+            b = reader.nextLine();
+            if (b.equals("#")) break;
+            q += b;
+        }
 
 
-        
+
     }
 }
