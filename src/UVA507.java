3ea9aab800ef209b2bbd2afee6b76fc801ec2ec0
Vishal Patel <vis8051@gmail.com>
2015-11-22 00:51:42 +1100

    WIP

--- src/UVA507.java
+++ src/UVA507.java
@@ -17,22 +17,19 @@ public class UVA507 {
             int k = in.nextInt();
             int a[] = new int[k];
             int sum = 0, start = 0 ,ans =0;
-            for ( int i = 0 ; i < k ; i++){
+            for ( int i = 0 ; i < k ; i++) {
                 a[i] = in.nextInt();
 
                 sum += a[i];
 
-                ans = Math.max(ans,sum);
+                ans = Math.max(ans, sum);
 
-                if ( sum < 0 ) {
-                    sum =0;
+                if (sum < 0) {
+                    sum = 0;
                     start = i;
                 }
-
-
+                
             }
-
-
 
         }
 
