5d14cf9b6684d22d31f194394a31ac6ab9643e7b
Vishal Patel <vis8051@gmail.com>
2015-11-22 00:48:57 +1100

    wip

--- src/UVA507.java
+++ src/UVA507.java
@@ -4,6 +4,7 @@
  */
 
 import java.util.Scanner;
+import java.math.*;
 
 public class UVA507 {
 
@@ -15,9 +16,23 @@ public class UVA507 {
         while ( t-- ) {
             int k = in.nextInt();
             int a[] = new int[k];
+            int sum = 0, start = 0 ,ans =0;
             for ( int i = 0 ; i < k ; i++){
                 a[i] = in.nextInt();
+
+                sum += a[i];
+
+                ans = Math.max(ans,sum);
+
+                if ( sum < 0 ) {
+                    sum =0;
+                    start = i;
+                }
+
+
             }
+
+
 
         }
 
