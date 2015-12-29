7c369c145e51048d1943494349256e7487a70a9a
Vishal Patel <vis8051@gmail.com>
2015-12-30 01:53:07 +1100

    UVA10189 mineswipper

--- UVA/UVA10189/Main.java
+++ UVA/UVA10189/Main.java
@@ -9,6 +9,7 @@ public class Main {
 
     public  static  void  main(String[] args){
 
+        int[][] dir = {{-1,-1},{0,-1},{1,-1},{1,0},{1,1},{0,1},{-1,1},{-1,0}};
         Scanner in = new Scanner (System.in);
         while ( true ){
             Integer a = in.nextInt();
@@ -17,12 +18,56 @@ public class Main {
             if ( a == 0 && b == 0 )
                 break;
 
-
-            char[][] mine = new char[a][b];
+            int [][] mine = new int[a][b];
 
             for ( int i = 0 ; i < a ; i++ ) {
-                for ( int j = 0 ; j < b ; j++ ){
-                    mine[i][j] = in.next().charAt(0);
+                for ( int j = 0 ; j < b ; j++ ) {
+                    int k = in.nextInt();  //in.next().charAt(0);
+                    if ( k == '.'){
+                        mine[i][j] = 0;
+                    }else {
+                        mine[i][j] = k;
+                    }
+                }
+            }
+
+
+            for ( int i = 0 ; i < a ; i++ ) {
+                for ( int j = 0 ; j < b ; j++ ) {
+
+                    if ( mine[i][j] != '*' ){
+                        if ( i == 0 && j == 0 ) {
+                            if ( mine[i+1][j] == '*')
+                                    mine[i][j]++;
+                            if ( mine[i+1][j+1] == '*')
+                                    mine[i][j]++;
+                            if ( mine[i][j+1] == '*')
+                                    mine[i][j] ++;
+                        }
+                        else if ( (i != 0 && j != a-1) && j == 0 ){
+                            if ( mine[i+1][j] == '*')
+                                mine[i][j]++;
+                            if ( mine[i+1][j+1] == '*')
+                                mine[i][j]++;
+                            if ( mine[i][j+1] == '*')
+                                mine[i][j] ++;
+                            if ( mine[i-1][j+1] == '*')
+                                mine[i][j] ++;
+                            if ( mine[i-1][j] == '*')
+                                mine[i][j] ++;
+                        }else if ( i == 0 && (j != 0 && j!= b-1 ){
+                            if ( mine[i][j-1] == '*')
+                                mine[i][j] ++;
+                            if ( mine[i+1][j-1] == '*')
+                                mine[i][j] ++;
+                            if ( mine[i+1][j] == '*')
+                                mine[i][j]++;
+                            if ( mine[i+1][j+1] == '*')
+                                mine[i][j]++;
+                            if ( mine[i][j+1] == '*')
+                                mine[i][j] ++;
+                        }
+                    }
                 }
             }
 
